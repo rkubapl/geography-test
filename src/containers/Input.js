c712a2abdf7015a16e32568bcdc985e740a6fe7f
rkubapl <kontakt@rkuba.pl>
2023-10-27 16:59:11 +0200

    Deleted placeholders (used for testing)

--- src/containers/Input.js
+++ src/containers/Input.js
@@ -6,7 +6,7 @@ const Input = params => {
     return (
         <div>
             <label htmlFor="fname">{data.label}</label>
-            <input autocomplete="off" type="text" id="fname" placeholder={data.correctAnswer} name="Nazwa" value={value} onChange={e => handleUpdateInput(data.label, e.target.value)} />
+            <input autocomplete="off" type="text" id="fname" name="Nazwa" value={value} onChange={e => handleUpdateInput(data.label, e.target.value)} />
             { showAnswer && (value === data.correctAnswer ? "Prawidłowa odpowiedź" : "Błędna odpowiedź") }
         </div>
     )
