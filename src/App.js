a0d9d6c5d2059a9e279107b32a8ada93fc9c7253
rkubapl <kontakt@rkuba.pl>
2023-10-27 17:14:55 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -4,23 +4,9 @@ import {useEffect, useState} from "react";
 import Input from "./containers/Input";
 
 function App() {
-    const locations = 10;
+    // const locations = 10;
 
-    // const originalPoints = [
-    //     {x: 200, y: 400, name: "Nizina Wielkopolska"},
-    //     {x: 500, y: 220, name: "Nizina Małopolska"},
-    //     {x: 300, y: 350, name: "Nizina Śląska"}
-    // ];
-
-    const [randomly, setRandomly] = useState([])
-    const [correctAnswersCount, setCorrectAnswersCount] = useState([])
-    const [showAnswer, setShowAnswer] = useState(false)
-
-    // const [isGameOver, setIsGameOver] = useState(false)
-
-    const [inputs, setInputs] = useState({})
-
-    const [points] = useState([
+    const originalPoints = [
         {x: 1252, y: 528, name: "Pobrzeże Koszalińskie"},
         {x: 2426, y: 800, name: "Pojezierze Mazurskie"},
         {x: 1223, y: 947, name: "Pojezierze Pomorskie"},
@@ -30,15 +16,26 @@ function App() {
         {x: 2812, y: 1469, name: "Nizina Południowopodlaska"},
         {x: 2814, y: 2097, name: "Wyżyna Lubelska"},
         {x: 2278, y: 2171, name: "Wyżyna Małopolska"},
-        {x: 1823, y: 2458, name: "Wyżyna Śląska"},
+        {x: 1750, y: 2458, name: "Wyżyna Śląska"},
+        {x: 1975, y: 2350, name: "Wyżyna Krakowsko-Częstochowska"},
         {x: 1025, y: 2506, name: "Sudety"},
-        {x: 2139, y: 2807, name: "Karpaty"},
+        {x: 2139, y: 2907, name: "Karpaty"},
         {x: 1810, y: 634, name: "Żuławy Wiślane"},
         {x: 1297, y: 1378, name: "Pojezierze Wielkopolskie"},
-        {x: 1630, y: 1764, name: "Nizina Południowowielkopolska"},
-        {x: 1359, y: 2302, name: "Nizina Śląska"},
+        {x: 1600, y: 1764, name: "Nizina Południowowielkopolska"},
+        {x: 1259, y: 2302, name: "Nizina Śląska"},
         {x: 2660, y: 2397, name: "Kotlina Sandomierska"},
-    ])
+    ]
+
+    const [randomly, setRandomly] = useState([])
+    const [correctAnswersCount, setCorrectAnswersCount] = useState([])
+    const [showAnswer, setShowAnswer] = useState(false)
+
+    // const [isGameOver, setIsGameOver] = useState(false)
+
+    const [inputs, setInputs] = useState({})
+
+    const [points] = useState(JSON.parse(JSON.stringify(originalPoints)));
 
     useEffect(() => {
         let correctAnswers = 0;
@@ -58,7 +55,7 @@ function App() {
     useEffect(() => {
         let copyPoints = points;
 
-        for (let i = 0; i < locations; i++) {
+        for (let i = 0; i < originalPoints.length+1; i++) {
             if(copyPoints.length === 0) break;
             let randomPoint = Math.floor(Math.random()*copyPoints.length);
 
@@ -82,13 +79,14 @@ function App() {
             <div className="questions">
                 {/*<p id="coords">tekst</p>*/}
                 <h1>Pytania</h1>
+                <span>Wpisz w pole tekstowe nazwy krain geograficznych odpowiadające podanym cyfrom na mapie po lewej stronie!<br/> Jeśli cyfry na mapie się nie załadowały odśwież stronę.</span>
                 {/*<input point={point} inputChange={value => input(point.label, value)} />*/}
                 {randomly.map(point =>
                     <Input showAnswer={showAnswer} data={{correctAnswer: point.name, label: point.label}} value={inputs[point.label]} handleUpdateInput={input} />
                 )
                 }
                 <button onClick={() => setShowAnswer(true)} >Pokaż odpowiedzi</button>
-                {showAnswer && (<p>Uzyskano {correctAnswersCount}/{points.length} punktów essa</p>)}
+                {showAnswer && (<p>Uzyskano {correctAnswersCount}/{randomly.length} punktów!<br/>Aby zagrać ponownie odśwież stronę.</p>)}
             </div>
         </div>
     </div>
