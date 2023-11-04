bcb83bcaa2006bab7d24a7b259acc580b67ce1d8
rkubapl <kontakt@rkuba.pl>
2023-11-04 19:48:02 +0100

    eslint fix

--- src/Poradnik.js
+++ src/Poradnik.js
@@ -6,10 +6,10 @@ export const Poradnik = () => {
             <Link to="/">Strona główna</Link>
             <h1>Poradnik</h1>
             <h2>1. Tryb testu</h2>
-            <img src="/legenda/test.png" width="75%" />
+            <img alt="Instrukcje do trybu testu" src="/legenda/test.png" width="75%" />
             <h2>2. Tryb nauki</h2>
             <span>Np. możesz zastanowić się nad nazwą lokalizacji, a potem kliknąć i sprawdzić czy dobrze myślałeś/aś.</span>
-            <img src="/legenda/nauka.png" width="75%" />
+            <img alt="Instrukcje do trybu nauku" src="/legenda/nauka.png" width="75%" />
             <br />
             <br />
             <span>Najlepiej połączyć obie te metody - pierwsza metoda pozwala generalnie zapamiętać lokalizacje, a druga umożliwia zapamiętanie nazw.</span>
