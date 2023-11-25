bd8d911651fb445fa9ef88d059c4a2e8607c4f20
rkubapl <kontakt@rkuba.pl>
2023-11-25 21:53:31 +0100

    mongodb fix

--- frontend/lib/mongodb.ts
+++ frontend/lib/mongodb.ts
@@ -1,36 +1,33 @@
-//stolen from https://github.com/vercel/mongodb-starter/blob/main/lib/mongodb.ts
-import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI as string; // your mongodb connection string
-const options = {};
-
-declare global {
-    var _mongoClientPromise: Promise<MongoClient>;
+if (!process.env.MONGODB_URI) {
+    throw new Error('Invalid environment variable: "MONGODB_URI"')
 }
 
-class Singleton {
-    private static _instance: Singleton;
-    private client: MongoClient;
-    private clientPromise: Promise<MongoClient>;
-    private constructor() {
-        this.client = new MongoClient(uri, options);
-        this.clientPromise = this.client.connect();
-        if (process.env.NODE_ENV === 'development') {
-            // In development mode, use a global variable so that the value
-            // is preserved across module reloads caused by HMR (Hot Module Replacement).
-            global._mongoClientPromise = this.clientPromise;
-        }
-    }
+const uri = process.env.MONGODB_URI
+const options = {}
 
-    public static get instance() {
-        if (!this._instance) {
-            this._instance = new Singleton();
-        }
-        return this._instance.clientPromise;
-    }
+let client
+let clientPromise: Promise<MongoClient>
+
+if (!process.env.MONGODB_URI) {
+    throw new Error('Please add your Mongo URI to .env.local')
 }
-const clientPromise = Singleton.instance;
+
+if (process.env.NODE_ENV === 'development') {
+    // In development mode, use a global variable so that the value
+    // is preserved across module reloads caused by HMR (Hot Module Replacement).
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(uri, options)
+        global._mongoClientPromise = client.connect()
+    }
+    clientPromise = global._mongoClientPromise
+} else {
+    // In production mode, it's best to not use a global variable.
+    client = new MongoClient(uri, options)
+    clientPromise = client.connect()
+}
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
-export default clientPromise;
+export default clientPromise
